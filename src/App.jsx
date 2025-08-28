89e9701748041f1faae2fded80e9f9dd9a9ada53
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-08-28 20:36:04 -0300

    add routing and pages for Cards and Tabela; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Cards from './pages/Cards'
+import Tabela from './pages/Tabela'
 
 function App() {
  
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Cards />} />
+      <Route path="/tabela" element={<Tabela />} />
+    </Routes>
      
     </>
   )
