4d7e6971c05bc7cd6749a3166f4801905dfd9751
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-08-28 21:05:53 -0300

    add Tabela component with product table and styling

--- src/pages/Tabela.jsx
+++ src/pages/Tabela.jsx
@@ -1,7 +1,44 @@
 export default function Tabela() {
-    return(
-        <div>
-            <h1>Tabela</h1>
-        </div>
-    )
+  return (
+    <main>
+      <h1>Tabela de Produtos</h1>
+      <table className="tabela">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Tipo</th>
+            <th>Marca</th>
+            <th>Voltagem</th>
+            <th>Ano</th>
+            <th>Preço</th>
+            <th>Peso (kg)</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+          </tr>
+
+          <tr>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+            <td>produto</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
 }
